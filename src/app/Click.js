6ca723e5f33ebaebb8576f3a60ff8c4df4837780
meetdesai10 <dmeet1008@gmail.com>
2023-09-30 00:02:30 +0530

    jsjn

--- src/app/Click.js
+++ src/app/Click.js
@@ -7,16 +7,17 @@ export default function Click() {
   }
   const Nested = () => {
     return (
-        <>
-            <h1>Hello i am nested component</h1>
-        </>
-    )
+      <>
+        <h1>Hello i am nested component</h1>
+      </>
+    );
   };
   return (
     <div>
+      <h1>hello my name is meet desai</h1>
       <h1>My name is {name}</h1>
       <button onClick={fruits}>Click me</button>
-      <Nested/>
+      <Nested />
       {Nested()}
     </div>
   );
