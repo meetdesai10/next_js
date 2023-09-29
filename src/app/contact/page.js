1e3a2093a321c5a36f1bf6fa4855d8ab59b39f06
meetdesai10 <dmeet1008@gmail.com>
2023-09-30 01:24:12 +0530

    dss

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function contactpage() {
   return (
     <div>
-      <h1>Contact page</h1>
+      <h1>Contact pages</h1>
       <Link href="/">Go to home page</Link>
     </div>
   )
